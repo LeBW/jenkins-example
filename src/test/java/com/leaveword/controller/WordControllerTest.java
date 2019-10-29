207d522c4721381b514ba0a303d0c90ccd6285df
ALDRICH1234 <1215937913@qq.com>
2019-10-29 20:38:04 +0800

    alan wu

--- src/test/java/com/leaveword/controller/WordControllerTest.java
+++ src/test/java/com/leaveword/controller/WordControllerTest.java
@@ -1,16 +1,64 @@
 package com.leaveword.controller;
 
+import com.leaveword.service.WordService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class WordControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    @Autowired
+    WordService wordService;
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
-    @Test
-    public void leaveWord() {
+    @After
+    public void tearDown() throws Exception {
     }
 
     @Test
-    public void getWords() {
+    public void leaveWord() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/leaveWord")
+                                              .accept(MediaType.APPLICATION_JSON)
+                                              .param("userId", "1")
+                                              .param("title", "Jack001")
+                                              .param("content", "Jack001"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(wordService.leaveWord(1,"Jack001","Jack001").getStatus()))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.content").value(wordService.leaveWord(1,"Jack001","Jack001").getContent()))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    public void getWords() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/getWords")
+                                      .accept(MediaType.APPLICATION_JSON)
+                                      .param("userId", "1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(wordService.getWords(1).getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value(wordService.getWords(1).getContent()))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
     }
 }
