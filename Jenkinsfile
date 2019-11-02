884907aaf9ba37e775391fc602782a0f262b117c
LeBW <27091925@qq.com>
2019-11-02 19:00:14 +0800

    modify jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -98,5 +98,3 @@ node {
         postAlways()
     }
 }
-
-
--- src/main/java/com/leaveword/DemoApplication.java
+++ src/main/java/com/leaveword/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+		System.out.println("New Feuture");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
